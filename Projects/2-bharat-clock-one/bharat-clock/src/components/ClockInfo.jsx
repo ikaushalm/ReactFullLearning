e162bfcbf27801ab8a275e1d39e0a21536eff263
KaushalPatel <kaushal.patel@zigram.tech>
2025-04-12 12:32:21 +0530

    new project added of projecy bharat

---
+++ Projects/2-bharat-clock-one/bharat-clock/src/components/ClockInfo.jsx
@@ -0,0 +1,8 @@
+function ClockInfo() {
+  return (
+    <div className="clock-info">
+      <h2>This is the class that shows time in Bharat at all times</h2>
+    </div>
+  );
+}
+export default ClockInfo;
---
+++ Projects/2-bharat-clock-one/bharat-clock/src/components/ClockName.jsx
@@ -0,0 +1,5 @@
+function ClockName() {
+  return <h1 className="clock-name-text">Bharat Clock</h1>;
+}
+
+export default ClockName;
